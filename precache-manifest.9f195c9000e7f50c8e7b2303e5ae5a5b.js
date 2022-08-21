2437908ad1d7239051e52b3a96d72d57034a0779
Syed007Hassan <k201052@nu.edu.pk>
2022-08-21 22:18:06 +0500

    Updates

---
+++ precache-manifest.9f195c9000e7f50c8e7b2303e5ae5a5b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "343a587e2eed310b29ca",
+    "url": "/KeeperApp/static/js/main.343a587e.chunk.js"
+  },
+  {
+    "revision": "ae6142dcdf3d0e7cfa94",
+    "url": "/KeeperApp/static/js/1.ae6142dc.chunk.js"
+  },
+  {
+    "revision": "8db71a8c03bb9e2a1c6f",
+    "url": "/KeeperApp/static/js/runtime~main.8db71a8c.js"
+  },
+  {
+    "revision": "a41a5a73c259370245b25cade2385078",
+    "url": "/KeeperApp/index.html"
+  }
+];
